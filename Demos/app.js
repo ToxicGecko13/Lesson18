15a8f94a37fb345e27c756de7855d43d7ccb0fe4
Carsten Ruhl <ToxicGecko13@Gmail.com>
2019-02-19 16:54:21 -0800

    Demo Code

---
+++ Demos/app.js
@@ -0,0 +1,13 @@
+const vue = new Vue({
+    el: '#app',
+    data: {
+        title: 'Hello World',
+        url: 'https://www.google.com/',
+        userName: '',
+    },
+    methods: {
+        greet(name) {
+            return `Welcome to my world ${name}`;
+        }
+    }
+})
